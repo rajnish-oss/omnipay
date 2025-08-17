9bf7f0047bbcee12e5e618c83a631f6d5ae57a91
rajnish.oss <rajnishpandey844@gmail.com>
2025-08-17 10:29:19 +0530

    m4

--- app/v1/checkout/sessions/route.ts
+++ app/v1/checkout/sessions/route.ts
@@ -1,7 +1,8 @@
 export const dynamic = "force-dynamic";
 
-import Stripe from "stripe";
 import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
 
 const stripe = new Stripe(process.env.SECRET_KEY as string);
 
